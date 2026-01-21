945971c53ec5f0831cf4cd6657a4afe49fb61c5c
anderskrag <anderskrag03@gmail.com>
2026-01-21 10:02:57 +0100

    La til en ny funk

--- area.py
+++ area.py
@@ -4,3 +4,7 @@ import numpy as np
 def circle_area(radius: np.ndarray) -> np.ndarray:
     """Calculate the area of a circle given its radius."""
     return np.pi * radius ** 2
+
+def quad_area(a):
+    """Calculate the area of a quadrat with sides equal a"""
+    return a**2
